87a69f06e3c108b91e636673818ccb9fd6dd17db
Sandydht <sandydwi75@gmail.com>
2023-06-30 23:25:03 +0700

    feat: switch lambda and kata kunci yield

--- src/SwitchStatement.java
+++ src/SwitchStatement.java
@@ -6,12 +6,26 @@
 * - Kondisi di switch statement hanya untuk perbandingan ==
 */
 
+/*
+* Switch Lambda
+*
+* - Di Java versi 14, diperkenalkan switch expression dengan lambda
+* - Ini lebih mempermudah saat penggunaan switch expression karena kita tidak perlu menggunakan kata kunci break
+*/
+
+/*
+* Kata Kunci Yield
+*
+* - Di Java 14, ada kata kunci baru yaitu yield, dimana kita menggunakan kata kunci yield untuk mengembalikan nilai pada switch statement.
+* - Ini sangat mempermudah kita ketika butuh membuat data berdasarkan kondisi switch statement.
+*/
+
 public class SwitchStatement {
   public static void main(String[] args) {
     /*
     * Kode: Switch Statement
     */
-    var nilai = "B";
+    var nilai = "A";
 
     switch (nilai) {
       case "A":
@@ -28,5 +42,44 @@ public class SwitchStatement {
         System.out.println("Mungkin anda salah jurusan");
         break;
     }
+
+    /*
+    * Kode: Switch Lambda
+    */
+    switch (nilai) {
+      case "A" -> System.out.println("Anda lulus dengan baik");
+      case "B", "C" -> System.out.println("Anda lulus");
+      case "D" -> System.out.println("Anda tidak lulus");
+      default -> {
+        System.out.println("Mungkin anda salah jurusan");
+      }
+    }
+
+    /*
+    * Kode: Kata Kunci Yield
+    */
+    String ucapan;
+    switch (nilai) {
+      case "A" -> ucapan = "Anda lulus dengan baik";
+      case "B", "C" -> ucapan = "Anda lulus";
+      case "D" -> ucapan = "Anda tidak lulus";
+      default -> ucapan = "Mungkin anda salah jurusan";
+    }
+    System.out.println(ucapan);
+
+    /*
+    * Kode: Switch dengan yield
+    */
+    ucapan = switch (nilai) {
+      case "A":
+        yield "Anda lulus dengan baik";
+      case "B", "C":
+        yield "Anda lulus";
+      case "D":
+        yield "Anda tidak lulus";
+      default:
+        yield "Mungkin anda salah jurusan";
+    };
+    System.out.println(ucapan);
   }
 }
